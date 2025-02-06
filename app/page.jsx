f5b366ca160c11f557c85bc56ddcdae1f12536bd
riyaz <riyaz3582@gmail.com>
2025-02-06 16:26:25 +0600

    installed and linked prisma

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import prisma from '../lib/prisma'
+async function getPosts() {
+    const posts = await prisma.post.findMany({
+        where:{published: true},
+        include:{
+            author:{
+                select:{name:true}
+            }
+        }
+    })
+    return posts;
+}
+export default async function page  ()  {
+    const posts = await getPosts()
+    console.log(posts)
+    return (
+        <div>
+            <p>feed</p>
+        </div>
+    );
+};
+
+ 
